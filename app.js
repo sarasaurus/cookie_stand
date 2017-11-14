0e791f26ec60cc749ef8bd610b47dd23687876e0
Sarah Bixler <bixler.s.e@gmail.com>
2017-11-14 14:34:16 -0800

    changed variables to be more semantic

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 'use strict';
 //global variables
 var hours = ['6am', '7am', '8am', '9am', '10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
-var output = [];
+var hourlySales = [];
 var totalSales = 0;
 var stores = [];
 
 //store object
-function Store (location, min, max, avgCookie) {
+function Store (location, min, max, avgSales) {
   this.name = location;
   this.min = min;
   this.max = max;
-  this.avgCookie = avgCookie;
+  this.avgSales = avgSales;
 }
 //populating storeLocations
 
 
 //METHODS
-Store.custPerHour = function () {
+Store.randNum = function () {
   return Math.random() * (this.max - this.min) + this.min;
 };
 
-Store.prototype.outputProduct = function () {
-  Math.round (this.avgCookie * custPerHour());
+Store.prototype.dailySales = function () {
+  Math.round (this.avgSales * randNum());
 };
 
 Store.prototype.salesPerHour = function () {
   for (var i = 0; i <= hours.length; i++) {
-    output.push (outputProduct);//pushing product to the array 'output'
-    totalSales += outputProduct;
+    hourlySales.push (dailySales);//pushing product to the array 'hourlySales'
+    totalSales += dailySales;
 }
 };
 /*
@@ -37,7 +37,7 @@ Store.prototype.writeToPage = function () {
 
   for (var j = 0; j < this.hours.length; j++) {
     var child = document.createElement('li');
-    child.textContent = this.hours[j] + ': ' + this.output[j];
+    child.textContent = this.hours[j] + ': ' + this.hourlySales[j];
     parent.appendChild(child);
   }
   childTotal.textContent = 'TOTAL: ' + this.totalSales;
