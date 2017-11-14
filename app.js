46001711e3bcfcb6facfe3ff9b86814333409c54
Sarah Bixler <bixler.s.e@gmail.com>
2017-11-14 07:45:15 -0800

    added commentary, still working on adding totals

--- app.js
+++ app.js
@@ -6,32 +6,34 @@ var storePike = {
   hours:['6am', '7am', '8am', '9am', '10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'],
   total: 0,
 
-//the below doesn't work but I don't know why another idea is to use push
-  /*total: output.reduce( function(total, amount){
-    return total + amount
-  }),*/
-
-  custPerHour: function getRandomArbitrary() {
+  custPerHour: function getRandomArbitrary() {//getting random number of customers that vist per hour
     return Math.random() * (this.max - this.min) + this.min;
   },
-  salesPerHour: function () {
+  salesPerHour: function () {//multiplying the number of customers by the avg cookie sale/customer
     //console.log(this.avgCookie);
     for (var i = 0; i <= this.hours.length; i++) {
-      this.output.push (Math.round (this.avgCookie * this.custPerHour()));
+      this.output.push (Math.round (this.avgCookie * this.custPerHour()));//pushing product to the array 'output'
     }
   },
-
+  /*calcTotal: function () {
+    for (var i = 0; i <= this.hours.length; i++) {
+      this.output[i] += this.output[i];
+    }
+  },*/
+  total: output.reduce( function(total, amount){//trying to add up all places in output
+    return total + amount
+  }),
   writeToPage: function () {
     for (var j = 0; j < this.hours.length; j++) {
-      var parent = document.getElementById('pike');
-      var child = document.createElement('li');
-      child.textContent = this.hours[j] + ': ' + this.output[j];
-      parent.appendChild(child);
+      var parent = document.getElementById('pike');//where we put
+      var child = document.createElement('li');//what we add, a <li> element with....
+      child.textContent = this.hours[j] + ': ' + this.output[j];//this content
+      parent.appendChild(child);//now add the <li>text elements succesively to the parent element
     }
   }
 
 };
-storePike.salesPerHour();
+storePike.salesPerHour();//need to call functions for them to run
 storePike.writeToPage();
 
 var storeSeaTac = {
