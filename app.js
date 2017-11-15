fe7874561559a83affb8c54680de45cd674c5234
Sarah Bixler <bixler.s.e@gmail.com>
2017-11-14 17:06:14 -0800

    got my constructor object to populate

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 //global variables
 var hours = ['6am', '7am', '8am', '9am', '10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
 var stores = [];
+//var alki = 'location', 34, 34, 34;
 
 //store object
 function Store (location, min, max, avgSales) {
@@ -11,55 +12,54 @@ function Store (location, min, max, avgSales) {
   this.avgSales = avgSales;
   this.hourlySales = [];
   this.totalSales = 0;
+  stores.push(this);
+  console.log('what store: ', this);
+
 }
 
 //populating storeLocations
-var pike = new Store ('1st and Pike', 23, 65, 6.3);
-var seatac = new Store ('SeaTac Airport', 3, 24, 1.2);
-var seacent = new Store ('Seattle Center', 11, 38, 3.7);
-var caphill = new Store ('Capitol Hill', 20, 38, 2.3);
-var alki = new Store ('Alki', 2, 16, 4.6);
-
+new Store ('1st and Pike', 23, 65, 6.3);
+new Store ('SeaTac Airport', 3, 24, 1.2);
+new Store ('Seattle Center', 11, 38, 3.7);
+new Store ('Capitol Hill', 20, 38, 2.3);
+new Store ('Alki', 2, 16, 4.6);
 
 //METHODS
-Store.prototype.addtoList = function (){
-  stores.push(this);
-
-};
 Store.prototype.randNum = function () {
   return Math.random() * (this.max - this.min) + this.min;
 };
 
 Store.prototype.dailySales = function () {
-  Math.round (this.avgSales * this.randNum());
+  return Math.round (this.avgSales * this.randNum());
 };
 
 Store.prototype.salesPerHour = function () {
   for (var i = 0; i <= hours.length; i++) {
-    this.hourlySales.push (this.dailySales);//pushing product to the array 'hourlySales'
-    this.totalSales += this.dailySales;
-}
+    this.hourlySales.push (this.dailySales());//pushing product to the array 'hourlySales'
+    this.totalSales += this.hourlySales[i];
+  }
+
 };
 
-Store.prototype.writeToPage = function () {
+for (var i = 0; i <= stores.length; i++) {
+  stores[i].salesPerHour();
+}
 
-  var tblEl = document.getElementById('tbody');
-  var thEl = document.createElement('th');
+
+//WRITE TO PAGE
+Store.prototype.createRow = function () {
+
+  var tblEl = document.getElementById('hours-tbl');
   var trEl = document.createElement('tr');
-  var tdEl = document.createElement('td');
 
-  for (var i = 0; i < stores.length; i++) {
+  /*for (var i = 0; i < stores.length; i++) {
     trEl.textContent = this.location;
     trEl.appendChild(tblEl);
   }
-
+*/
   for (var j = 0; j < hours.length; j++) {
-    thEl.textContent = hours[j];
-    thEl.appendChild(trEl);
+    trEl.textContent = hours[j];
+    tblEl.appendChild(trEl);
   }
 
-
-  tdEl.textContent = 'TOTAL: ' + this.totalSales;
-  tdEl.appendChild(tdEl);
 };
-store.
