e159d1d786e14588a5250efbf4c7721f76b56ea0
Sarah Bixler <bixler.s.e@gmail.com>
2017-11-15 15:34:27 -0800

    got info from input into constructor

--- app.js
+++ app.js
@@ -1,26 +1,4 @@
 'use strict';
-
-// first we define a variable for where we want browser to listen
-var formEl = document.getElementById('main-form');
-
-//here we are creating our handler
-//and attaching the collection of data to an event, here: onSubmit, so when submit button is pressed, is our event
-function onSubmit(event) {//event is a predefined function!
-  event.preventDefault();//no lose data on refresh
-  var newStore = {
-    location: event.target.location.value,
-    min: parseInt(event.target.min.value, 10),
-    max: parseInt(event.target.max.value, 10),
-    avgCookie: parseInt(event.target.avgCookie.value, 10),
-  };
-
-  console.log('my form data', newStore);//loggin info stored in newStore Array
-}
-
-formEl.addEventListener('submit', onSubmit);//we are calling and attaching browser's listener to formEl (so everything in our form will be piquing browsers interest)
-
-//NOW ADDING OBJ CONSTRUCTOR
-
 //global variables
 var hours = ['6am', '7am', '8am', '9am', '10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
 var stores = [];
@@ -34,9 +12,25 @@ function Store (location, min, max, avgSales) {
   this.hourlySales = [];
   this.totalSales = 0;
   stores.push(this);
+  //this.salesPerHour();
   console.log('what store: ', this);
 
 }
+
+//INPUT FORM
+var formEl = document.getElementById('main-form');
+function onSubmit(event) {//event is a predefined function!
+  event.preventDefault();//no lose data on refresh
+  var newStore = {
+    location: event.target.location.value,
+    min: parseInt(event.target.min.value, 10),
+    max: parseInt(event.target.max.value, 10),
+    avgCookie: parseInt(event.target.avgCookie.value, 10),
+  };
+  new Store (newStore.location, newStore.min, newStore.max, newStore.avgCookie);
+}
+formEl.addEventListener('submit', onSubmit);
+
 
 //Store Info
 new Store ('1st and Pike', 23, 65, 6.3);
@@ -64,7 +58,7 @@ Store.prototype.salesPerHour = function () {
   }
 };
 
-//populating my store cards
+//populating my store cards, NO NEED?
 for (var i = 0; i < stores.length; i++) {
   stores[i].salesPerHour();
 }
