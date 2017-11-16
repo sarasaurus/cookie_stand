4ef9eba3ee5ab98c4311c0fe163faa1b02d2fa26
Sarah Bixler <bixler.s.e@gmail.com>
2017-11-16 09:49:19 -0800

    took away hour 8 noticed problem with input for decimal!

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 //global variables
-var hours = ['6am', '7am', '8am', '9am', '10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
+var hours = ['6am', '7am', '8am', '9am', '10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
 var stores = [];
 //GLOBAL METHODS
 var randNum = function (min, max) {
